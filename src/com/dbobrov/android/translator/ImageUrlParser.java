7cf6c5d91a71af89bed052b3b79060f6ebc9bbe8
Dmitry Bobrov <me@dbobrov.com>
2012-10-13 16:44:42 +0400

    Created interfaces ImageUrlParser and WordTranslator

---
+++ src/com/dbobrov/android/translator/ImageUrlParser.java
@@ -0,0 +1,5 @@
+package com.dbobrov.android.translator;
+
+public interface ImageUrlParser {
+	public String[] getImageUrls(String word);
+}
---
+++ src/com/dbobrov/android/translator/WordTranslator.java
@@ -0,0 +1,5 @@
+package com.dbobrov.android.translator;
+
+public interface WordTranslator {
+	public String GetTranslation(String word);
+}
