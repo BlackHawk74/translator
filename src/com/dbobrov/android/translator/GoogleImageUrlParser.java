22e8974fc2ada80c446b478785bce69fdfe15e92
Konsolerr <zayats1812@mail.ru>
2012-10-16 19:35:43 +0400

    GoogleImageParser changed (now works)

--- src/com/dbobrov/android/translator/GoogleImageUrlParser.java
+++ src/com/dbobrov/android/translator/GoogleImageUrlParser.java
@@ -1,50 +1,74 @@
 package com.dbobrov.android.translator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Arrays;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.util.*;
+
+import android.util.Base64;
 
 public class GoogleImageUrlParser implements ImageUrlParser {
 	@Override
 	public String[] getImageUrls(String word) {
 		try {
 			URL url = new URL(
-					"https://www.google.ru/search?num=10&hl=ru&newwindow=1&safe=off&site=imghp&tbm=isch&source=hp&biw=1366&bih=667&q="
-							+ word);
+					"https://www.google.ru/search?num=10&hl=ru&newwindow=1&safe=off&site=imghp&tbm=isch&source=hp&biw=1366&bih=624&q="
+							+ word
+							+ "#q="
+							+ word
+							+ "&num=10&hl=ru&newwindow=1&safe=off&site=imghp&tbm=isch&source=lnt&tbs=isz:i&sa=X&ei=YHF9UL-WNbGO4gSKs4H4DQ&ved=0CCMQpwUoAw&bav=on.2,or.r_gc.r_pw.r_qf.&fp=69f22befd53748ff&bpcl=35277026&biw=1366&bih=667");
+			Properties systemSettings = System.getProperties();
+			systemSettings.put("proxySet", "true");
+			systemSettings.put("http.proxyHost", "1.1.1.14");
+			systemSettings.put("http.proxyPort", "3128");
+
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			String encodedUserPwd = Base64.encodeToString(
+					"Логин:пароль".getBytes(), 0);
+			con.setRequestProperty("Proxy-Authorization", "Basic "
+					+ encodedUserPwd);
+			con.setRequestMethod("GET");
+
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					url.openStream()));
+					con.getInputStream()));
 			String line;
-			String[] ans = new String[10];
+			ArrayList<String> ans = new ArrayList<String>();
 			int totalCount = 0;
+			int firstMet;
+//			line = reader.readLine();
+//			PrintWriter out = new PrintWriter("/mnt/sdcard/text.txt");
+//			out.println(line);
 
 			while ((line = reader.readLine()) != null && totalCount < 10) {
 				int ind = 0;
-				int firstMet;
-				while ((firstMet = line.indexOf("data-src=", ind)) != -1) {
-					int closing = line.indexOf("\"", ind + 10);
-					String partOfAns = line.substring(ind + 10, closing);
-					ans[totalCount++] = partOfAns;
+				while ((firstMet = line.indexOf(
+						"http://www.google.com/imgres?imgurl=", ind)) != -1) {
+					int closing = line.indexOf("&amp;imgrefurl", firstMet + 35);
+					String partOfAns = line.substring(firstMet + 36, closing);
+					ans.add(partOfAns);
 					ind = closing;
+					totalCount++;
 				}
 
 			}
+			
+			String[] realAns = new String[totalCount];
+			ans.toArray(realAns);
+			
 			reader.close();
 			if (totalCount == 0) {
 				return null;
-			}
-			if (totalCount < 10) {
-				return Arrays.copyOfRange(ans, 0, totalCount);
-			}
-			if (totalCount == 10) {
-				return ans;
+			} else {
+				return realAns;
 			}
 		} catch (MalformedURLException e) {
-			// nothing
+			e.printStackTrace();
 		} catch (IOException e) {
-			// nothing
+			e.printStackTrace();
 		}
 		return null;
 
