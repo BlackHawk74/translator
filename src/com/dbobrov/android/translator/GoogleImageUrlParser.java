c44dc30aa56c4fd17b96d6b87862bf9b9d04c227
Konsolerr <zayats1812@mail.ru>
2012-10-16 20:14:59 +0400

    FINAL CHANGE OF GoogleImageUrlParser

--- src/com/dbobrov/android/translator/GoogleImageUrlParser.java
+++ src/com/dbobrov/android/translator/GoogleImageUrlParser.java
@@ -3,50 +3,32 @@ package com.dbobrov.android.translator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
 
-import android.util.Base64;
 
 public class GoogleImageUrlParser implements ImageUrlParser {
 	@Override
 	public String[] getImageUrls(String word) {
 		try {
 			URL url = new URL(
-					"https://www.google.ru/search?num=10&hl=ru&newwindow=1&safe=off&site=imghp&tbm=isch&source=hp&biw=1366&bih=624&q="
+					"https://www.google.com/search?as_st=y&tbm=isch&as_q="
 							+ word
-							+ "#q="
-							+ word
-							+ "&num=10&hl=ru&newwindow=1&safe=off&site=imghp&tbm=isch&source=lnt&tbs=isz:i&sa=X&ei=YHF9UL-WNbGO4gSKs4H4DQ&ved=0CCMQpwUoAw&bav=on.2,or.r_gc.r_pw.r_qf.&fp=69f22befd53748ff&bpcl=35277026&biw=1366&bih=667");
-			Properties systemSettings = System.getProperties();
-			systemSettings.put("proxySet", "true");
-			systemSettings.put("http.proxyHost", "1.1.1.14");
-			systemSettings.put("http.proxyPort", "3128");
-
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			String encodedUserPwd = Base64.encodeToString(
-					"Логин:пароль".getBytes(), 0);
-			con.setRequestProperty("Proxy-Authorization", "Basic "
-					+ encodedUserPwd);
-			con.setRequestMethod("GET");
+							+ "&as_epq=&as_oq=&as_eq=&cr=&as_sitesearch=&safe=active&orq=&tbs=isz:m&biw=1304&bih=683&sei=n359UIuSG8jU4QTSvYHYBw");
 
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					con.getInputStream()));
+					url.openStream()));
 			String line;
 			ArrayList<String> ans = new ArrayList<String>();
 			int totalCount = 0;
 			int firstMet;
-//			line = reader.readLine();
-//			PrintWriter out = new PrintWriter("/mnt/sdcard/text.txt");
-//			out.println(line);
 
 			while ((line = reader.readLine()) != null && totalCount < 10) {
 				int ind = 0;
 				while ((firstMet = line.indexOf(
-						"http://www.google.com/imgres?imgurl=", ind)) != -1) {
+						"http://www.google.com/imgres?imgurl=", ind)) != -1
+						&& totalCount < 10) {
 					int closing = line.indexOf("&amp;imgrefurl", firstMet + 35);
 					String partOfAns = line.substring(firstMet + 36, closing);
 					ans.add(partOfAns);
@@ -55,10 +37,10 @@ public class GoogleImageUrlParser implements ImageUrlParser {
 				}
 
 			}
-			
+
 			String[] realAns = new String[totalCount];
 			ans.toArray(realAns);
-			
+
 			reader.close();
 			if (totalCount == 0) {
 				return null;
